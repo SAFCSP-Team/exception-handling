7b00f982a8eeb4e3f1529429a5cebb9ccad53d3b
devAlq <khalidalqdev@gmail.com>
2023-12-09 16:58:12 +0300

    expection handeled

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
     int[] myNumbers = {1, 2, 3};
+    try{
     System.out.println(myNumbers[10]);
 
-  }
+  } catch (ArrayIndexOutOfBoundsException e) {
+  System.println("index out of range")
 }
+
